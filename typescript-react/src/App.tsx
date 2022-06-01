61a63852d5183558606245f28a352c0b47ae5f73
wesselpawel <wesselpawel@wp.pl>
2022-06-01 17:06:36 +0200

    MultiStepForm completed

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -6,16 +6,30 @@ import './App.css';
 function App() {
   const [currentStep, setCurrentStep] = useState(1)
   return (
-    <div className="app">
-      <Header title="Join us!" color="black"/>
-      <form>
-        <MultiStepForm currentStep={currentStep}/>
-      </form>
-        <div className="dots-containter">
-          <button className='dot-btn' id="1" onClick={() => setCurrentStep(1)}></button>
-          <button className='dot-btn' id="2" onClick={() => setCurrentStep(2)}></button>
+    <>
+    <div className="background"></div>
+      <div className="app">
+        <div className="form-wrapper">
+          <Header title="Join us!" color="black"/>
+          <form>
+            <MultiStepForm currentStep={currentStep}/>
+          </form>
+            <div className="nav-containter">
+              {currentStep === 1 ? (
+                <button className='navigate-btn' onClick={() => setCurrentStep(2)}>Next</button>
+              ) : (
+                <button className='navigate-btn' onClick={() => console.log("submitted a form")}>
+                  Submit
+                </button>
+              )}
+              <div className="dots">
+                <button className={currentStep === 1 ? 'dot-btn current' : 'dot-btn'} onClick={() => setCurrentStep(1)}></button>
+                <button className={currentStep === 2 ? 'dot-btn current' : 'dot-btn'} onClick={() => setCurrentStep(2)}></button>
+              </div>
+            </div>
         </div>
-    </div>
+      </div>
+    </>
   );
 }
 
--- typescript-react/src/components/MultiStepForm/MultiStepForm.tsx
+++ typescript-react/src/components/MultiStepForm/MultiStepForm.tsx
@@ -1,25 +1,45 @@
+import {useState} from 'react'
+
 export interface FormProps {
     currentStep: number
-    onChange: React.ChangeEventHandler<HTMLInputElement>
 }
 
+type State = {
+    firstName: string
+    lastName: string | null
+    userName: string
+    password: string
+}
+
+const initialState: State = {
+    firstName: "",
+    lastName: "",
+    userName: "",
+    password: ""
+} 
+
 const MultiStepForm = (props: FormProps) => {
+    const [state, setState] = useState(initialState)
+
+    const handleChange = <T extends keyof State>(prop: T, value: State[T]) => {
+        setState({...state, [prop]: value})
+    }
     return (
         <>
                 {props.currentStep === 1 ?
                     (
                         <div className="inputs">
                             <label htmlFor="firstName">Firstname:</label>
-                            <input type="text" id="firstName" onChange={props.onChange}/>
+                            <input type="text" id="firstName" value={state.firstName} onChange={(e) => handleChange("firstName", e.target.value)}/>
                             <label htmlFor="lastName">Lastname:</label>
-                            <input type="text" id="lastName"/>
+                            <input type="text" id="lastName" value={state.lastName || ""} onChange={(e) => handleChange("lastName", e.target.value)}/>
                         </div>
                     ) : (
                         <div className="inputs">
                             <label htmlFor="userName">Username:</label>
-                            <input type="text" id="userName"/>
-                            <label htmlFor="userPass">Password:</label>
-                            <input type="password" id="userPass"/>
+                            <input type="text" id="userName" value={state.userName} onChange={(e) => handleChange("userName", e.target.value)}/>
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" id="password" value={state.password} onChange={(e) => handleChange("password", e.target.value)}/>
                         </div>
                     )
                 }
