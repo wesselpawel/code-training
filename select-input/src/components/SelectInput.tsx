80483bdc511ced1de62e2503da7fccdbf3ec440f
Sebastian Rosloniec <sebastian.rosloniec@gmail.com>
2022-06-16 00:36:55 +0100

    Code feedback
    All good; only some advice on code style - which you normally pick up once you've started working in a team. Won't hurt to build good habits though.
    
    I recommend getting some practice with BEM and SCSS. No need to learn every single rule - just get familiar with the patterns. Nowadays, most devs use Tailwind but let's start by building a solid foundation.

--- select-input/src/components/SelectInput.tsx
+++ select-input/src/components/SelectInput.tsx
@@ -1,43 +1,59 @@
 import { useState } from 'react';
 
-export interface Props {
-    listItems: { itemName: string, id: number}[]
+// Extract repeated code (from lines 7 and 15)
+type ListItem = { name: string, id: number} // the prefix "item" in "itemName" is redundant as the name of the object is alreday "(list)Item"
+
+export interface SelectInputProps {
+    listItems: ListItem[]
     color: string
     bgColor: string
 }
 
-const SelectInput = (props: Props) => {
-    const [selectedItem, selectedItemSet] = useState<{ itemName: string, id: number }>(props.listItems[0])
-    const [menuOpened, menuOpenedSet] = useState<boolean>(false)
-    const newSelectedItem = (itemName: string, id: number ) => {
-        selectedItemSet({itemName, id} )
-        menuOpenedSet(false)
+// Nowadays, most devs destructure props either in the parameters on the first line of component body
+// "listItems = []" - default objects/arrays/function values to avoid destructuring errors (e.g. undefined[0]) which crash the app
+const SelectInput = ({listItems = [], color, bgColor}: SelectInputProps): JSX.Element => {
+    const [selectedItem, setSelectedItem] = useState<ListItem>(listItems[0]) // state setter names begin with "set"
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
+
+    const selectItem = (item: ListItem ): void => {
+        setSelectedItem(item)
+        setMenuOpen(false)
     }
+    
+    const toggleMenuOpen = (): void => setMenuOpen(!menuOpened) // This is not necessary to extract as it's redefined on each render either way; just a matter of code style
+    
     return (
         <div className='wrapper'>
-        <h1>Coffee type selected: {selectedItem.itemName} <br /> with id: {selectedItem.id}</h1>
+            <h1>
+                /* Keep "<br/>" for paragraph breaks only; use CSS everywhere else */
+                <span className="block">Coffee type selected: {selectedItem.name}</span>
+                <span className="block">with id: {selectedItem.id}</span>
+            </h1>
+            
             <div className="select-box">
                 <div
                     className="selected-item"
-                    onClick={() => menuOpenedSet(!menuOpened)}
+                    onClick={toggleMenuOpen}
                 >
-                    {selectedItem.itemName}
-                    <div className='arrow'> &gt;
-                    </div>
+                    {selectedItem.name}
+                    <div className='arrow'> &gt;</div>
                 </div>
-                    <div className={menuOpened === true ? 'menu opened' : 'menu closed'}>
-                        {props.listItems.map((item, index) =>
-                            <div
-                                className='item'
-                                key={index}
-                                onClick={() => newSelectedItem(item.itemName, item.id)}
-                            >
-                                {item.itemName}
-                                {selectedItem.id === item.id ? (<div className="checked-item"></div>) : (undefined)}
-                                
-                            </div>
-                        )}
-                    </div>
+                
+                /* Ternary operator coerces types to booleans, so `menuOpen` is equivalent to `menuOpened === true`  */ 
+                <div className={menuOpen ? 'menu menu-opened' : 'menu menu-closed'}> /* Look into BEM (quick) */
+                    {listItems.map((item, index) =>
+                        <div
+                            className='item'
+                            key={index}
+                            onClick={() => selectItem(item)}
+                        >
+                            {item.name}
+                            /* For conditions that fallback on falsey valuues ('undefined', 'false', etc.), you can use "&&" instead of ternary */   
+                            /* Childless elements can use self-closing tags, e.g. `<div/>` */          
+                            {selectedItem.id === item.id && <div className="checked-item" />}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
