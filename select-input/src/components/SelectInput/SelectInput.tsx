9b348f759b3826a017d0edf5ac1af145ad85e99e
wesselpawel <wesselpawel@wp.pl>
2022-06-17 01:12:39 +0200

    BEM methodology

--- select-input/src/components/SelectInput/SelectInput.tsx
+++ select-input/src/components/SelectInput/SelectInput.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import './styles/style.css';
 // Extract repeated code (from lines 7 and 15)
 type ListItem = { name: string, id: number} // the prefix "item" in "itemName" is redundant as the name of the object is alreday "(list)Item"
 
@@ -20,42 +20,36 @@ const SelectInput = ({listItems = [], color, bgColor}: SelectInputProps): JSX.El
         setMenuOpen(false)
     }
     
-    const toggleMenuOpen = (): void => setMenuOpen(!menuOpened) // This is not necessary to extract as it's redefined on each render either way; just a matter of code style
+    const toggleMenuOpen = (): void => setMenuOpen(!menuOpen) // This is not necessary to extract as it's redefined on each render either way; just a matter of code style
     
     return (
-        <div className='wrapper'>
-            <h1>
-                /* Keep "<br/>" for paragraph breaks only; use CSS everywhere else */
-                <span className="block">Coffee type selected: {selectedItem.name}</span>
-                <span className="block">with id: {selectedItem.id}</span>
-            </h1>
-            
-            <div className="select-box">
+
+            <div className="select">
                 <div
-                    className="selected-item"
+                    className="select__chosen"
                     onClick={toggleMenuOpen}
                 >
                     {selectedItem.name}
-                    <div className='arrow'> &gt;</div>
+                    <div className='select__arrow'> &gt;</div>
                 </div>
                 
-                /* Ternary operator coerces types to booleans, so `menuOpen` is equivalent to `menuOpened === true`  */ 
-                <div className={menuOpen ? 'menu menu-opened' : 'menu menu-closed'}> /* Look into BEM (quick) */
+                {/*Ternary operator coerces types to booleans, so `menuOpen` is equivalent to `menuOpened === true`*/}
+                <div className={menuOpen ? 'select__menu--opened' : 'select__menu'}> {/* Look into BEM (quick) */}
                     {listItems.map((item, index) =>
                         <div
-                            className='item'
+                            className='select__item'
                             key={index}
                             onClick={() => selectItem(item)}
                         >
                             {item.name}
-                            /* For conditions that fallback on falsey valuues ('undefined', 'false', etc.), you can use "&&" instead of ternary */   
-                            /* Childless elements can use self-closing tags, e.g. `<div/>` */          
-                            {selectedItem.id === item.id && <div className="checked-item" />}
+                            {/* For conditions that fallback on falsey valuues ('undefined', 'false', etc.), you can use "&&" instead of ternary */}   
+                            {/* Childless elements can use self-closing tags, e.g. `<div/>` */}    
+                            {selectedItem.id === item.id && <div className="select__item--checked" />}
                         </div>
                     )}
                 </div>
             </div>
-        </div>
+
     )
 }
 
